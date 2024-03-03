f82c6cdc10d573642089a578c8979391de572123
a <a@a-300E4M-300E4S-300E4L>
2024-03-03 13:02:30 -0300

    commit inicial projeto mentoria Java

---
+++ src/main/java/jdev_mentoria_lojavirtual/LojaVirtualMentoriaApplication.java
@@ -0,0 +1,13 @@
+package jdev_mentoria_lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
+	}
+
+}
